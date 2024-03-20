28e208c9e0fc9483d7e674b9f1aa7e2599a5bad6
Gabriel Aguirre <ga33aguirre33@gmail.com>
2024-03-20 20:14:58 -0300

    El juego ahora tiene tres niveles de dificultad

--- game.py
+++ game.py
@@ -2,6 +2,8 @@ import random
 # Lista de palabras posibles
 words = ["python", "programación", "computadora", "código", "desarrollo",
 "inteligencia"]
+# Lista de vocales
+vocales = ["a","e","i","o","u", "á","é","í","ó","ú"]
 # Elegir una palabra al azar
 secret_word = random.choice(words)
 # Número máximo de intentos permitidos
@@ -9,14 +11,37 @@ max_attempts = 10
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 print("¡Bienvenido al juego de adivinanzas!")
+# Menu para seleccionar la dificultad
+print("-"*25)
+print("Seleccione una dificultad")
+print("1. Facil\n2. Media\n3. Dificil")
+print("-"*25)
+opcion = int(input())
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
-word_displayed = "_" * len(secret_word)
+# Evaluar opcion.
+txt = "es :"
+letters = []
+if opcion == 1:
+    guessed_letters += vocales
+    for letter in secret_word:
+        if letter in guessed_letters:
+            letters.append(letter)
+        else:
+            letters.append("_")
+    word_displayed = "".join(letters)
+    print(word_displayed)
+elif opcion == 2:
+    word_displayed = secret_word[0] + "_" * (len(secret_word) - 2) + secret_word[-1]
+    txt = "La palabra comienza y termina de la siguiente manera: "
+else:
+    word_displayed = "_" * len(secret_word)
 # Mostrarla palabra parcialmente adivinada
-print(f"Palabra: {word_displayed}")
+print(f"La palabra {txt} {word_displayed}")
 i = 0
-while i < 10:
+while i < 10: # El programa solo fallará cuando se ingrese una letra equivocada.
     # Pedir al jugador que ingrese una letra
     letter = input("Ingresa una letra: ").lower()
+    print(letter)
     # Verificar si la letra ya ha sido adivinada
     if letter in guessed_letters:
         print("Ya has intentado con esa letra. Intenta con otra.")
